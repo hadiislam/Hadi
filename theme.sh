60170e5a0d892e170d3207e1a4b0735e031cdebd
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-01 03:34:51 +0600

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,2 +1,16 @@
+cd
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+cd mysetup
+cd asset
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd
+$HOME
+rm -rf .termux
+cd
+cd mysetup
+cp .termux /$HOME
+cd
 echo "alias chcolor='$HOME/.termux/colors.sh'"
 echo "alias chfont='$HOME/.termux/fonts.sh'"
+clear
