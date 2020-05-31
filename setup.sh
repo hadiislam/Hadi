673178a8a7747c7ee971c397dffdd0647d23b1b6
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-05-31 20:37:22 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd
 apt update
 apt upgrade -y
-apt install -y python ruby python2 
+apt install -y python ruby
 gem install lolcat
 git clone https://github.com/hadiislam/CyberCat
 clear
@@ -9,5 +9,26 @@ apt install toilet -y
 apt install figlet -y
 apt install mc -y
 clear
+cd
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+cd mysetup
+cd asset
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd
+apt install figlet
+apt install ruby
+gem install lolcat
+cd
+$HOME
+rm -rf .termux
+mkdir .termux
+cd
+cd mysetup
+cd theme
+cd dracula
+cp colors.properties ~/.termux
+cd
+clear
 echo
-figlet DONE | lolcat
+figlet I'm Ready For You | lolcat
