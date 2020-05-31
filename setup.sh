17ea30f847f4fbd3c22039cffcc0a63ecb95a522
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-05-31 20:06:33 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd
+apt update
+apt upgrade -y
+apt install -y python ruby python2 
+gem install lolcat
+git clone https://github.com/hadiislam/CyberCat
+clear
+apt install toilet -y
+apt install figlet -y
+apt install mc -y
+clear
+echo
+figlet DONE | lolcat
