c06f1d83091d62028e66a2d7336bccb273305a86
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-06 02:48:00 +0600

    Create up.py

---
+++ up.py
@@ -0,0 +1,19 @@
+
+#update Termux
+
+else:
+          os.system("rm -rf "+system.bin+"/CAT")
+          os.system("rm -rf "+system.conf_dir+"/CAT")
+        os.system("clear")
+os.system(" cd")
+os.system("git clone https://github.com/hadiislam/CAT")
+os.system("cd CAT")
+os.system("chmod +x *)
+os.system("mkdir "+system.conf_dir+"/CAT")
+          os.system("cp -r modules core CAT.py "+system.conf_dir+"/CAT")
+          os.system("cp -r core/CAT "+system.bin)
+          os.system("cp -r core/CAT "+system.bin)
+          os.system("chmod +x "+system.bin+"/CAT")
+          os.system("cd .. && rm -rf CAT")
+        logo.exit()
+break
