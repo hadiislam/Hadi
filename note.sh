312f244b913336c0950b6cc6620b17483feffcc0
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-06 19:18:20 +0600

    Update note.sh

--- note.sh
+++ note.sh
@@ -2,9 +2,9 @@ echo CAT NOTE
 echo
 echo BY HADI ISLAM
 echo
-read -p"[NAME]: " name
-read -p"[Category]: " cat
-read -p"[URL]: " url
+read -p"[NAME]: " varname
+read -p"[Category]: " varcat
+read -p"[URL]: " varurl
 
 echo " 
         "'$name'" : {
@@ -15,11 +15,3 @@ echo "
 		 "dependency" : ["git"]
 		 }, 
 " > note.txt
-		
-cat "note.txt" >> /data/data/com.termux/files/home/mysetup/cnote.md
-cd
-rm note.txt
-clear
-cd
-cd mysetup
-sh note.sh
