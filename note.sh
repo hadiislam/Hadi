86505d7a3009c9ca1f14158687c86c379fdaecf6
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-06 19:38:17 +0600

    Update note.sh

--- note.sh
+++ note.sh
@@ -2,15 +2,15 @@ echo CAT NOTE
 echo
 echo BY HADI ISLAM
 echo
-read -p"[NAME]: " varname
-read -p"[Category]: " varcat
-read -p"[URL]: " varurl
+read -p"[NAME]: "varname
+read -p"[Category]: "varcat
+read -p"[URL]: "varurl
 
 echo " 
-        "'$varname'" : {
-         "name" : "'$varname'",
-		 "package_name" : "'$varname'",
-		 "category" : ["'$varcat'"],
+        "$varname" : {
+         "name" : "$varname",
+		 "package_name" : "$varname",
+		 "category" : ["$varcat"],
 		 "url" : "$varurl",
 		 "dependency" : ["git"]
 		 }, 
